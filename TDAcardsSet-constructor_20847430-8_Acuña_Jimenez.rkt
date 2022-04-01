4909e3dcab5862c31c771e1ea44b50c71d3a76db
Dieggo19 <diego.acuna.j@usach.cl>
2022-04-01 19:04:09 -0300

    agregue  estructura para el tda

---
+++ TDAcardsSet-constructor_20847430-8_Acuña_Jimenez.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+;TDA cardsSet - constructor. Función constructora de conjuntos válidos de cartas para el juego Dobble.
+
+
+;REPRESENTACION
+
+
+;CONSRUCTOR
+
+
+;PERTENENCIA
+
+
+;SELECTOR
+
+
+;MODIFICADOR
+
+
+;OTRAS OPERACIONES
