5aeaf667d67eb5fa2d5fb2dd7cd3e99e78e009ac
Dieggo19 <diego.acuna.j@usach.cl>
2022-04-01 19:15:25 -0300

    agregue estructura para el tda cardsSet - dobble?

---
+++ TDAcardsSet-dobble_20847430-8_Acuña_Jimenez.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+;TDA cardsSet - dobble?: Función que permite verificar si el conjunto de cartas en el conjunto corresponden a un conjunto válido.
+
+
+;REPRESENTACION
+
+
+;CONSRUCTOR
+
+
+;PERTENENCIA
+
+
+;SELECTOR
+
+
+;MODIFICADOR
+
+
+;OTRAS OPERACIONES
