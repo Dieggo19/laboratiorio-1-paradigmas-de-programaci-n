05e044e81f3f73d552227bafaddd5085d5516655
Dieggo19 <diego.acuna.j@usach.cl>
2022-04-17 17:04:01 -0400

    cree la funcion que entrega la primera carta del mazo

--- TDAcardsSet-constructor_20847430-8_Acuña_Jimenez.rkt
+++ TDAcardsSet-constructor_20847430-8_Acuña_Jimenez.rkt
@@ -4,9 +4,24 @@
 
 
 ;REPRESENTACION
-
+;una lista con sub-listas las cuales representarian las cartas del mazo
+(define lista '(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57))
 
 ;CONSRUCTOR
+
+
+
+; funcion que crear la primera carta del mazo
+; dom: list
+; rec: list
+
+(define firstcarta (lambda (lista iterador)
+                     (cond
+                       [(= (simboloCarta(length lista)) iterador)
+                        null]
+                       [else (cons (list-ref lista iterador) (firstcarta lista (+ iterador 1)))]))) ;se utiliza list-ref para poder jugar con cualquier simbolo
+
+;funcion que crea las primeras n cartas del mazo
 
 
 ;PERTENENCIA
@@ -19,3 +34,11 @@
 
 
 ;OTRAS OPERACIONES
+
+; Función que calcula los símbolos de las cartas implementando una ecuacion de segundo grado
+(define simboloCarta (lambda (n)
+                      (+ (/ (- (sqrt (- (* 4 n) 3)) 1) 2) 1)))
+
+; Función que da la cantidad total de simbolos implemetada por el algoritmo entregado
+(define totalSimbolos (lambda (n)
+                       (+ (+ (* n n) n) 1)))
